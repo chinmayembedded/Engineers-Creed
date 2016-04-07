9ac7b3d2e6aa12b2124bd36791ee613d7592a346
chinmayembedded <chinmayssawant@live.com>
2016-04-08 02:57:42 +0800

    Update Quotes.php

--- Quotes.php
+++ Quotes.php
@@ -11,9 +11,33 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<style>
+		h1{
+			    padding-top: 4%;
+				padding-left: 7%;
+				font-size: -webkit-xxx-large;
+				
+				color: whitesmoke;
+		}
+		p{
+			padding-top: 4%;
+			padding-left: 7%;
+			color: white;
+			font-size: x-large;
+		}
+	</style>
 </head>
 
-<body background="images/under.jpg">
-
+<body style="background-color:black">
+	<h1>
+		QUOTE
+	</h1>
+	<p>
+		Project we have completed demonstrate what we know, Future projects decide what we will learn. 
+	</p>
+	
+	<p>
+		To the optimist glass is half full. To the pessimist, the glass is half empty. To the engineer, the glass is twice as big as it needs to be.
+	</p>
 </body>
 </html>
